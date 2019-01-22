e2a9abc09eb4d7b3def89d00690c65365c7bd0bf
Dan Masquelier <dancrew32@gmail.com>
2019-01-21 22:17:15 -0800

    ipwhois on specified file app.py e.g. /var/log/nginx/access.log

---
+++ app.py
@@ -0,0 +1,37 @@
+import collections
+import ipwhois
+import pprint
+import re
+import socket
+import sys
+import warnings
+
+warnings.filterwarnings(action='ignore')
+
+MOST_COMMON = 10
+
+
+def scan(path):
+    with open(path, 'r') as f:
+        data = f.read()
+    IP_MATCH = '^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}'
+    return re.findall(IP_MATCH, data, flags=re.MULTILINE)
+
+
+def lookup(ip):
+    try:
+        socket.gethostbyaddr(ip)
+    except socket.herror:
+        return None
+    obj = IPWhois(ip)
+    return obj.lookup_whois()
+
+
+if __name__ == '__main__':
+    ips = scan(sys.argv[1])
+    c = collections.Counter(ips)
+    top = c.most_common(MOST_COMMON)
+
+    for ip, freq in top:
+        print(f'{ip} has {freq} entries.')
+        pprint.pprint(lookup(ip))
---
+++ makefile
@@ -0,0 +1,8 @@
+venv:
+	virtualenv -p python3 venv
+
+deps:
+	./venv/bin/pip3 install -r requirements.txt
+
+run:
+	./venv/bin/python app.py
